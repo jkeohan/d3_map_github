adee92a0e7661b9b340ba156472f558ca257a3f0
Joe Keohah <jkeohan@gmail.com>
2015-02-26 10:43:26 -0500

    updated

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,5 +1,3 @@
-  <script src="http://d3js.org/d3.v3.min.js"></script>
-  <script src="http://d3js.org/topojson.v1.min.js"></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
 
 
